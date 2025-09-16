c6da0e11538c9c672b9ebb279c9afea263101844
franco <fluffins007@outlook.com>
2025-09-16 17:33:07 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+LINK="https://github.com/SrFluff/jizz/blob/main/jizz"
+APP="jizz"
+APPPATH="/usr/local/bin/$APP"
+PATHAPP="/usr/local/bin/"
+
+wget "$LINK"
+if [ -e "$APP" ]; then
+	chmod +x "$APP"
+fi
+
+if [ -e "$APPPATH" ]; then
+	echo "Existing install found, replace? [y/N]"
+	read answer
+	if [ "$answer" == "y" ]; then
+		rm "$APPPATH"
+		mv "jizz" "$PATHAPP"
+		echo "installed!"
+	else
+		echo "Installation canceled"
+		rm "$APP"
+	fi
+else
+	mv "$APP" "$PATHAPP"
+
+fi
